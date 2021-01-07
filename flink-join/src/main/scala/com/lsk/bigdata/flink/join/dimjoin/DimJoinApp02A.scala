69adbc998f602220d31d4a91c1816231b2967674
lvshaokang <lvshaokang@hotmail.com>
2021-01-08 01:46:08 +0800

    feat(): Temporal Table Join

---
+++ flink-join/src/main/scala/com/lsk/bigdata/flink/join/dimjoin/DimJoinApp02A.scala
@@ -0,0 +1,99 @@
+package com.lsk.bigdata.flink.join.dimjoin
+
+import org.apache.flink.api.common.functions.RichMapFunction
+import org.apache.flink.configuration.Configuration
+import org.apache.flink.shaded.guava18.com.google.common.cache.{CacheBuilder, CacheLoader, LoadingCache, RemovalListener, RemovalNotification}
+import org.apache.flink.streaming.api.scala.{StreamExecutionEnvironment, _}
+
+import java.util.concurrent.TimeUnit
+
+/**
+ * 热存储(维表数据存储在Redis/HBase/MySQL等)
+ * 模拟数据从socket加载
+ *
+ * 1. Cache减轻访问压力
+ *
+ * 优点: 维度数据量不受内存限制,可以存储很大的数据量
+ *
+ * 缺点: 因为维表在外部存储中,读取速度受限于外部存储的读取速度;另外维表的同步也有延迟
+ *
+ * User
+ * userName String,
+ * cityId Int,
+ * timestamp Long
+ *
+ * City
+ * cityId Int,
+ * cityName String,
+ * timestamp Long
+ */
+object DimJoinApp02A {
+  
+  def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+  
+    val baseStream = env.socketTextStream("localhost", 9000, '\n')
+      .map(line => {
+        val splits = line.split(",")
+        // userName, cityId
+        (splits(0), splits(1).toInt)
+      })
+  
+    val joinStream = baseStream.map(new RichMapFunction[(String,Int), (String, Int, String)] {
+      // <cityId, cityName>
+      var cache: LoadingCache[Int, String] = _
+  
+      override def open(parameters: Configuration): Unit = {
+        cache = CacheBuilder.newBuilder()
+          // 最多缓存个数,超过了就根据最近最少使用算法来移除缓存 LRU
+          .maximumSize(1000)
+          // 在更新后的指定时间后就回收
+          .expireAfterWrite(10, TimeUnit.MINUTES)
+          // 指定移除通知
+          .removalListener(new RemovalListener[Int, String] {
+            override def onRemoval(removalNotification: RemovalNotification[Int, String]): Unit = {
+              println(removalNotification.getKey + "被移除了,值为: " + removalNotification.getValue)
+            }
+          })
+          .build(new CacheLoader[Int, String] {
+            override def load(k: Int): String = {
+              val cityName = readFromHBase(k)
+              cityName
+            }
+          })
+      }
+  
+      override def map(value: (String, Int)): (String, Int, String) = {
+        // map 中进行主表和维表的关联
+        var cityName = ""
+        if (cache.get(value._2) != null) {
+          cityName = cache(value._2)
+        }
+        (value._1, value._2, cityName)
+      }
+    })
+    
+    joinStream.print()
+    
+    env.execute(this.getClass.getSimpleName)
+  }
+
+  def readFromHBase(key: Int) : String = {
+    //读取hbase
+
+    // 这里写死,模拟从HBase读数据
+    var map: collection.mutable.Map[Int, String] = Map(){}
+    map += (1001 -> "beijing")
+    map += (1002 -> "shanghai")
+    map += (1003 -> "chengdu")
+    map += (1004 -> "wuhan")
+
+    var cityName = ""
+    if (map.contains(key)) {
+      cityName = map(key)
+    }
+
+    cityName
+  }
+  
+}
---
+++ flink-join/src/main/scala/com/lsk/bigdata/flink/join/dimjoin/DimJoinApp04.scala
@@ -0,0 +1,76 @@
+package com.lsk.bigdata.flink.join.dimjoin
+
+import org.apache.flink.streaming.api.scala.{StreamExecutionEnvironment, _}
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.table.api.EnvironmentSettings
+import org.apache.flink.table.api.scala.StreamTableEnvironment
+import org.apache.flink.table.api.scala._
+import org.apache.flink.types.Row
+
+
+/**
+ * Temporal table function join (Temporal table 是持续变化表上某一时刻的视图,
+ *  Temporal table function 是一个表函数,传递一个时间参数, 返回Temporal table 这一指定时刻的视图)
+ *  可以将维度数据流映射为Temporal table,主流与这个Temporal table进行关联,可以关联到某一版本(历史上某一个时刻)的维度数据
+ *
+ * 模拟数据从socket加载
+ *  ProcessingTime
+ *
+ * 优点: 维度数据量可以很大,维度数据更新及时,不依赖外部存储,可以关联不同版本的维度数据
+ *
+ * 缺点: 仅支持Flink SQL API
+ *
+ * User
+ * userName String,
+ * cityId Int,
+ * timestamp Long
+ *
+ * City
+ * cityId Int,
+ * cityName String,
+ * timestamp Long
+ */
+object DimJoinApp04 {
+  
+  def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    val bsSettings = EnvironmentSettings.newInstance().useBlinkPlanner().build()
+    val tableEnv = StreamTableEnvironment.create(env, bsSettings)
+
+    val baseStream = env.socketTextStream("localhost", 9000, '\n')
+      .map(line => {
+        val splits = line.split(",")
+        // userName, cityId
+        (splits(0), splits(1).toInt)
+      })
+
+    val cityStream = env.socketTextStream("localhost", 9001, '\n')
+      .map(line => {
+        val splits = line.split(",")
+        // cityId, cityName
+        (splits(0).toInt, splits(1))
+      })
+
+    val userTable = tableEnv.fromDataStream(baseStream, 'user_name ,'city_id, 'ps.proctime)
+    val cityTable = tableEnv.fromDataStream(cityStream, 'city_id, 'city_name, 'ps.proctime)
+
+    // 定义一个TemporalTableFunction
+    val dimCity = cityTable.createTemporalTableFunction('ps, 'city_id)
+    // 注册表函数
+    tableEnv.registerFunction("dimCity", dimCity)
+
+    val result = tableEnv.sqlQuery(
+      s"""
+         | select u.user_name, u.city_id, d.city_name
+         | from ${userTable} as u,
+         | Lateral table (dimCity(u.ps)) d
+         | where u.city_id = d.city_id
+         | """.stripMargin)
+
+    val resultDs = tableEnv.toAppendStream[Row](result)
+
+    resultDs.print()
+
+    env.execute(this.getClass.getSimpleName)
+  }
+}
